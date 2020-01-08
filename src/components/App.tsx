5e499727ffb6e82f157b8a2f11cad877db0e8990
KoutaKawase <suhrkawase@gmail.com>
2020-01-08 22:32:42 +0900

    🚧 とりあえずリクエストは最低限できた

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { SearchContainer } from './search/SearchContainer';
 import { UserInfoContainer } from './userInfo/UserInfoContainer';
 
 type ClickEvent = React.MouseEvent<HTMLButtonElement>;
 type ChangeEvent = React.ChangeEvent<HTMLInputElement>;
 
+const useFetch = (url: string): any => {
+  const [data, updateData] = useState(undefined);
+
+  useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      const response = await fetch(url);
+      const json = await response.json();
+      updateData(json);
+    };
+    fetchData();
+  }, [url]);
+  return data;
+};
+
 export const App: React.FC = () => {
   const [inputValue, setInputValue] = useState('');
+
+  const URL = 'https://api.github.com/users/KoutaKawase/repos';
+  const result = useFetch(URL);
 
   const handleClick = (e: ClickEvent): void => {
     e.preventDefault();
@@ -23,6 +40,7 @@ export const App: React.FC = () => {
       <h1>Hello World</h1>
       <SearchContainer onClick={handleClick} onChange={handleChange} inputValue={inputValue} />
       <UserInfoContainer />
+      <p>{JSON.stringify(result)}</p>
     </div>
   );
 };
