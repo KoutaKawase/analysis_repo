9d96fb03f1105ec90ea525e998fb2a0853138a8b
KoutaKawase <suhrkawase@gmail.com>
2020-01-08 10:14:50 +0900

    🚧 Inputとボタンのガワを作成

--- src/components/search/SearchContainer.tsx
+++ src/components/search/SearchContainer.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { SearchInput } from './SearchInput';
+import { SearchButton } from './SearchButton';
 
 export const SearchContainer: React.FC = () => {
-  return <div>{'SearchContainer'}</div>;
+  return (
+    <div>
+      <SearchInput />
+      <SearchButton />
+    </div>
+  );
 };
