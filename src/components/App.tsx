f5f32954ab0492b06d22d2c07dba478157412310
KoutaKawase <suhrkawase@gmail.com>
2020-01-09 00:04:05 +0900

    🚧 全リポジトリのそれぞれの使用言語を配列として格納して取得できるようにした

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,6 +5,21 @@ import { UserInfoContainer } from './userInfo/UserInfoContainer';
 type ClickEvent = React.MouseEvent<HTMLButtonElement>;
 type ChangeEvent = React.ChangeEvent<HTMLInputElement>;
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const makeLanguagesArrayFromJson = (json: any): string[] => {
+  const usedLanguages: string[] = [];
+
+  if (!json) {
+    return [];
+  }
+  for (const repo of json) {
+    usedLanguages.push(repo.language);
+  }
+
+  return usedLanguages;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const useFetch = (url: string): any => {
   const [data, updateData] = useState(undefined);
 
@@ -16,14 +31,16 @@ const useFetch = (url: string): any => {
     };
     fetchData();
   }, [url]);
+
   return data;
 };
 
 export const App: React.FC = () => {
   const [inputValue, setInputValue] = useState('');
 
-  const URL = 'https://api.github.com/users/KoutaKawase/repos';
-  const result = useFetch(URL);
+  const URL = `https://api.github.com/users/KoutaKawase/repos`;
+  const resultJson = useFetch(URL);
+  const usedLanguages = makeLanguagesArrayFromJson(resultJson);
 
   const handleClick = (e: ClickEvent): void => {
     e.preventDefault();
@@ -34,13 +51,15 @@ export const App: React.FC = () => {
     setInputValue(e.target.value);
     console.log(inputValue);
   };
-
+  //TODO: keyにindexはクソなので修正
   return (
     <div>
       <h1>Hello World</h1>
       <SearchContainer onClick={handleClick} onChange={handleChange} inputValue={inputValue} />
       <UserInfoContainer />
-      <p>{JSON.stringify(result)}</p>
+      {usedLanguages.map((lang: string, index) => {
+        return <p key={index}>{lang}</p>;
+      })}
     </div>
   );
 };
