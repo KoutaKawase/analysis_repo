3f6db3ac482ac3d0d4bb865cf9ffe464e4dceb56
KoutaKawase <suhrkawase@gmail.com>
2020-01-08 18:33:36 +0900

    ♻️  SearchContainerにあったハンドラ群をAppに移した
    chartやuserInfoで使えるように大元のコンポーネントに移したかった

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SearchContainer } from './search/SearchContainer';
 import { UserInfoContainer } from './userInfo/UserInfoContainer';
 
+type ClickEvent = React.MouseEvent<HTMLButtonElement>;
+type ChangeEvent = React.ChangeEvent<HTMLInputElement>;
+
 export const App: React.FC = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleClick = (e: ClickEvent): void => {
+    e.preventDefault();
+    console.log('Click!');
+  };
+
+  const handleChange = (e: ChangeEvent): void => {
+    setInputValue(e.target.value);
+    console.log(inputValue);
+  };
+
   return (
     <div>
       <h1>Hello World</h1>
-      <SearchContainer />
+      <SearchContainer onClick={handleClick} onChange={handleChange} inputValue={inputValue} />
       <UserInfoContainer />
     </div>
   );
