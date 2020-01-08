274c5f56aba6f305a6a4198eafbfdbe00900faae
KoutaKawase <suhrkawase@gmail.com>
2020-01-08 10:48:01 +0900

    🚧 Containerのハンドラを子コンポーネントに渡して実行できるようにした

--- src/components/search/SearchContainer.tsx
+++ src/components/search/SearchContainer.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SearchInput } from './SearchInput';
 import { SearchButton } from './SearchButton';
 
 export const SearchContainer: React.FC = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    e.preventDefault();
+    console.log('clicked!');
+  };
+
   return (
     <form>
       <SearchInput />
-      <SearchButton />
+      <SearchButton onClick={handleClick} />
     </form>
   );
 };
