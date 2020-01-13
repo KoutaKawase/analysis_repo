9f1905b228504145585afa452d24a5c4be750cec
KoutaKawase <suhrkawase@gmail.com>
2020-01-13 21:14:18 +0900

    🚧 webpackの警告を消すためにaltを変更

--- src/components/userInfo/Avatar.tsx
+++ src/components/userInfo/Avatar.tsx
@@ -5,5 +5,5 @@ interface Props {
 }
 
 export const Avatar: React.FC<Props> = (props: Props) => {
-  return <img src={props.image} alt={'avatar image'} width={'120px'} height={'120px'} />;
+  return <img src={props.image} alt="user avatar" width={'120px'} height={'120px'} />;
 };
