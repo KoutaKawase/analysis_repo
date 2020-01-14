7aeaa8c67947d82e1c8b8a124c5d4202e6c0fb1c
KoutaKawase <suhrkawase@gmail.com>
2020-01-14 20:44:01 +0900

    ✨ userInfoのスタイリングをした

--- src/components/userInfo/UserName.tsx
+++ src/components/userInfo/UserName.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import './userName.css';
 
 interface Props {
   name: string;
 }
 export const UserName: React.FC<Props> = (props: Props) => {
-  return <p color="red">{props.name}</p>;
+  return (
+    <p>
+      USER ID: <span className="user-name">{props.name}</span>
+    </p>
+  );
 };
